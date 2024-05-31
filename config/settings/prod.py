29acf0e5db378965e74180ec3d541a6bda621c11
sungjae <tjdwo98a@gmail.com>
2024-05-31 17:35:07 +0900

    'satic_root'

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,4 +2,6 @@ from .base import *
 
 ALLOWED_HOSTS = ['54.180.235.13']
 
-SITE_ID = 5  # 또는 현재 사용하고 있는 사이트의 ID로 변경
+SITE_ID = 2  # 또는 현재 사용하고 있는 사이트의 ID로 변경
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
